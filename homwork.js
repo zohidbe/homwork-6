fc37a204515eced62d0cd2c66d792aec152fd031
zohidbe <zohaxaydarov@gmail.com>
2024-08-08 11:47:41 +0500

    first commit

---
+++ homwork.js
@@ -0,0 +1,50 @@
+// task 1
+
+// function uchXonaliTekshirish(son) {
+//     return son >= 100 && son <= 999;
+// }
+
+// let son = 123;
+// console.log("Berilgan son uch xonali: " + uchXonaliTekshirish(son));
+
+// task 2
+
+// function ikkiTasiTeng(a, b, c) {
+//     return a === b || a === c || b === c;
+// }
+
+// let a = 5, b = 5, c = 10;
+// console.log("Berilgan uchta butun sonlarning hech bo'lmaganda 2 tasi bir-biriga teng: " + ikkiTasiTeng(a, b, c));
+
+// /task3
+
+// function qaramaQarshiTekshirish(a, b, c) {
+//     return a === -b || a === -c || b === -c;
+// }
+
+// let a = 5, b = -5, c = 10;
+// console.log("Berilgan uchta butun sonning hech bo'lmaganda bir jufti o'zaro qarama-qarshi: " + qaramaQarshiTekshirish(a, b, c));
+
+// task 4
+
+// function barchaRaqamlarHarXil(son) {
+//     let yuzlar = Math.floor(son / 100);
+//     let onlar = Math.floor((son % 100) / 10);
+//     let birliklar = son % 10;
+//     return yuzlar !== onlar && yuzlar !== birliklar && onlar !== birliklar;
+// }
+
+// let son20 = 123;
+// console.log("Ushbu sonning barcha raqamlari har xil: " + barchaRaqamlarHarXil(son20));
+
+// task 5
+
+// function palindromTekshirish(son) {
+//     let yuzlar = Math.floor(son / 100);
+//     let birliklar = son % 10;
+//     return yuzlar === birliklar;
+// }
+
+// let son23 = 121;
+// console.log("Ushbu son chapdan o'qilganda ham, o'ngdan o'qilganda ham bir xil: " + palindromTeksh)
+
---
+++ main.js
@@ -0,0 +1,25 @@
+// function x(a,b) {
+//     if ((a + b) % 2 == 0) {
+//         return"juft";
+//     }
+//     return"toq";
+// }
+// console.log(x(10,20));
+// object 1
+
+
+// function myFunc(theObject) {
+//     theObject.make = "toyota";
+//   }
+  
+//   const mycar = {
+//     make: "Honda",
+//     model: "Accord",
+//     year: 1998,
+//   };
+  
+//   console.log(mycar.make); // "Honda"
+//   myFunc(mycar);
+//   console.log(mycar.make.year); // "Toyota"
+  
+
